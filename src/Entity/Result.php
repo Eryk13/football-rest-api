5b0e541c6f121e2211e264667d3e88c617f71937
Eryk <eryk9903@gmail.com>
2024-02-20 14:18:59 +0100

    add Result to ApiResource

--- src/Entity/Result.php
+++ src/Entity/Result.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ResultRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ResultRepository::class)]
+#[ApiResource]
 class Result
 {
     #[ORM\Id]
